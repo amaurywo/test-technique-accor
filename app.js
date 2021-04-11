8d183cc7903b9378721111c1a5eec3f1dc858c71
xavier.medioni@gmail.com <1Justbeme>
2021-04-11 15:16:31 +0200

    Exercise 2: findHotelNearbyWithBestOffer implementation

--- app.js
+++ app.js
@@ -44,13 +44,78 @@ function findHotelsNearby ( lat, lng, radius ) {
 }
 
 function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
-    // TODO implement me
-    return null;
+
+	// Every parameters are mandatory (should throw an exception, and should not exist in typescript)
+	if ( typeof lat == "undefined" ) return null;
+	if ( typeof lng == "undefined" ) return null;
+	if ( typeof radius == "undefined" ) return null;
+	if ( typeof date == "undefined" ) return null;
+
+	let hotelsNearBy = findHotelsNearby ( lat, lng, radius );
+	let hotelDictionary = helper.hotelDictionary ( hotelsNearBy, priceService.getPrices() || [] );
+
+	let bestOfferPerHotelArray = [];
+
+	for ( let key in hotelDictionary )
+	{
+		let hotel = hotelDictionary [ key ];
+
+		// If no offer, then skip
+		if ( hotel.offers.length === 0 ) continue;
+
+		// Filter hotel offers on fare (STANDARD) and date, and sort (price only, ascending)
+		let hotelOffers = hotel.offers
+			.filter ( offer => {
+				return ( offer.fare === "STANDARD" ) && ( offer.date === date );
+			})
+			.sort ( ( o1, o2 ) => {
+				return o1.price - o2.price;
+			} );
+
+		let bestOfferOnPrice = hotelOffers [ 0 ];
+
+		// Add best offer for this hotel
+		bestOfferPerHotelArray.push ( {
+			ridCode: key,
+			offer: bestOfferOnPrice,
+			distance: hotel.distance
+		} );
+	}
+
+	if ( bestOfferPerHotelArray.length === 0 ) return null;
+
+	// Then sort bestOfferPerHotelArray (price ascending first, distance ascending second)
+	bestOfferPerHotelArray = bestOfferPerHotelArray.sort ( ( h1, h2 ) => {
+		let deltaPrice = h1.offer.price - h2.offer.price;
+
+		if ( deltaPrice !== 0 ) return deltaPrice;
+
+		return h1.distance - h2.distance;
+	} );
+
+	// The result is the first element of bestOfferPerHotelArray
+	const chosen = bestOfferPerHotelArray [ 0 ];
+	const ridCode = chosen [ "ridCode" ];
+	const hotel = hotelDictionary [ ridCode ];
+
+	// Only expected properties are exposed
+
+	const result = {
+		ridCode,
+		countryCode: hotel.countryCode,
+		localRating: hotel.localRating,
+		address: hotel.address,
+		commercialName: hotel.commercialName,
+		distance: hotel.distance,
+		offer: chosen.offer
+	};
+
+	return result;
 }
 
 function findHotelNearbyWithBestOfferForUser(lat, lng, radius, date, userId) {
-    // TODO implement me
-    return null;
+	// TODO implement me
+	return null;
 }
 
 module.exports = {
--- services/helper.js
+++ services/helper.js
@@ -6,15 +6,63 @@ const distance = (lat1, lon1, lat2, lon2) => {
 	const Δλ = (lon2-lon1) * Math.PI/180;
 
 	const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-			  Math.cos(φ1) * Math.cos(φ2) *
-			  Math.sin(Δλ/2) * Math.sin(Δλ/2);
+		Math.cos(φ1) * Math.cos(φ2) *
+		Math.sin(Δλ/2) * Math.sin(Δλ/2);
 	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
 	const d = R * c; // in metres
-	
+
 	return d;
 }
 
+/**
+ *
+ * @param hotels: { ridCode: string, ... }
+ * @param prices: { prices: [ { ridCode: string, offers: [] } ] }
+ * @returns hash [ key: ridCode ] = {} // hotel extended elements
+ */
+
+const hotelDictionary = ( hotels, prices ) => {
+
+	// Using ridCode as identifier
+	let dictionary = {};
+
+	// Build dictionary / hash from hotels dataset, using ridCode as key
+	hotels.forEach ( hotel => {
+		const key = hotel.ridCode;
+
+		/*
+		if ( typeof dictionary [ key ] != "undefined" )
+		{
+			// Warn if key conflict ?
+			console.warn ( "ridCode conflict: ", key );
+		}
+		*/
+
+		// If conflict => override (last declared has priority)
+		dictionary [ key ] = hotel;
+
+		// Extend with offers property, initialized to [] (no offer)
+		dictionary [ key ].offers = [];
+	});
+
+	// Extend with prices dataset
+	prices.forEach ( price => {
+
+		let key = price.ridCode;
+
+		// Only extends if ridCode exists in the dictionary
+		if ( typeof dictionary [ key ] != "undefined" )
+		{
+			// With concat, we avoid the override usecase, when prices array contains a same ridCode multiple times
+			dictionary [ key ].offers = dictionary [ key ].offers.concat ( price.offers );
+		}
+	});
+
+	return dictionary;
+}
+
 module.exports = {
-	distance: distance
+	distance: distance,
+	hotelDictionary: hotelDictionary
 }
