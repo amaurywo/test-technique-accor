2560a5b72b8a77a8677c86fab73e0fdd65bfcfd0
IzadZ <izad.zandi@gmail.com>
2021-10-22 16:09:02 +0200

    step 1 done

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const userService = require('./services/user-service');
-const hotelService = require('./services/hotel-service');
-const priceService = require('./services/price-service');
-const helper = require('./services/helper');
+const userService = require("./services/user-service");
+const hotelService = require("./services/hotel-service");
+const priceService = require("./services/price-service");
+const helper = require("./services/helper");
 
 function findHotelsNearby(lat, lng, radius) {
-    // TODO implement me
-	return [];
+  if (!lat || !lng || !radius) {
+    return [];
+  }
+  return hotelService.getHotels().reduce((hotels, hotel) => {
+    const distance = helper.distance(lat, lng, hotel.latitude, hotel.longitude);
+    if (distance <= radius) {
+      hotel.distance = distance;
+      delete hotel.latitude;
+      delete hotel.longitude;
+      hotels.push(hotel);
+    }
+    return hotels;
+  }, []);
 }
 
 function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
-    // TODO implement me
-    return null;
+  // TODO implement me
+  return null;
 }
 
 module.exports = {
-	findHotelsNearby: findHotelsNearby,
-	findHotelNearbyWithBestOffer: findHotelNearbyWithBestOffer
-}
+  findHotelsNearby: findHotelsNearby,
+  findHotelNearbyWithBestOffer: findHotelNearbyWithBestOffer,
+};
